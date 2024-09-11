dca7e78d7ff61dfc42cab6ba316053737fbb6741
Shadab0072 <shadab223382@gmail.com>
2024-09-11 10:12:54 +0530

    123

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,24 @@ import { faGithub, faInstagram, faTelegram, faWhatsapp } from '@fortawesome/free
 function Footer() {
   return (
     <section className="w-full overflow-x-hidden flex flex-col items-center gap-4 justify-evenly text-white myNewfont bg-black py-8">
+
       <div className="text-2xl font-light tracking-wider uppercase text-center px-4" style={{ wordSpacing: '1rem', letterSpacing: '0.3em' }}>
         Hang onto your memories
-      </div>
+      </div> 
 
       <div className="flex flex-col md:flex-row items-center gap-10 md:gap-20 text-center">
 
         {/* Social Media Links */}
         <div className="flex flex-row items-center gap-4 p-4 border-b-2 md:border-b-0 md:border-r-2">
-          <Link to={'https://www.instagram.com/husain_not_hussain/'} target='_blank'>
+          <Link to={''} target='_blank'>
             <FontAwesomeIcon icon={faInstagram} size="2x" className="text-white" />
           </Link>
 
-          <Link to={'https://api.whatsapp.com/send/?phone=%2B919956309404&text&type=phone_number&app_absent=0'} target='_blank'>
+          <Link to={''} target='_blank'>
             <FontAwesomeIcon icon={faWhatsapp} size="2x" className="text-white" />
           </Link>
 
-          <Link to={'https://github.com/Shadab0072'} target='_blank'>
+          <Link to={''} target='_blank'>
             <FontAwesomeIcon icon={faGithub} size="2x" className="text-white" />
           </Link>
         </div>
